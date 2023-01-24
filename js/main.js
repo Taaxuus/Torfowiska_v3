99bf16d1cc4db7b7a68e555d4587642dfe82ce22
Cezary Draus <108001877+Taaxuus@users.noreply.github.com>
2023-01-24 13:31:24 +0100

    work

--- js/main.js
+++ js/main.js
@@ -1,53 +1,53 @@
-const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-const allNavItems = document.querySelectorAll('.nav__item')
-const navBtnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll('.section')
-const footerYear = document.querySelector('.footer__year')
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
-	nav.classList.toggle('nav--active')
+	nav.classList.toggle('nav--active');
 
-	navBtnBars.classList.remove('black-bars-color')
+	navBtnBars.classList.remove('black-bars-color');
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
-			nav.classList.remove('nav--active')
-		})
-	})
+			nav.classList.remove('nav--active');
+		});
+	});
 
-	handleNavItemsAnimation()
-}
+	handleNavItemsAnimation();
+};
 
 const handleNavItemsAnimation = () => {
-	let delayTime = 0
+	let delayTime = 0;
 
 	allNavItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's'
-		delayTime++
-	})
-}
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
 
 //AutomaticYear
 const handleCurrentYear = () => {
-	const year = new Date().getFullYear()
-	footerYear.innerText = year
-}
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 
 //Function Observer
 
-const handleObserver = () => {
-	const currentSection = window.scrollY
-	allSections.forEach(section => {
-		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-			navBtnBars.classList.add('black-bars-color')
-		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-			navBtnBars.classList.remove('black-bars-color')
-		}
-	})
-}
+// const handleObserver = () => {
+// 	const currentSection = window.scrollY
+// 	allSections.forEach(section => {
+// 		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+// 			navBtnBars.classList.add('black-bars-color')
+// 		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+// 			navBtnBars.classList.remove('black-bars-color')
+// 		}
+// 	})
+// }
 
-navBtn.addEventListener('click', handleNav)
-handleCurrentYear()
-window.addEventListener('scroll', handleObserver)
+navBtn.addEventListener('click', handleNav);
+handleCurrentYear();
+window.addEventListener('scroll', handleObserver);
